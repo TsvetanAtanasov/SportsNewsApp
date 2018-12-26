c83fb2d8698d90e1cc0a27cdb75b800066db660a
TsvetanAtanasov <tzatanasov@gmail.com>
2018-12-26 19:25:03 +0200

    Created SandBox app for testing

--- src/Data/SportsNews.Data.Models/SportsNewsUser.cs
+++ src/Data/SportsNews.Data.Models/SportsNewsUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace SportsNews.Web.Areas.Identity.Data
+namespace SportsNews.Web.Areas.Identity.Data    
 {
     // Add profile data for application users by adding properties to the SportsNewsUser class
     public class SportsNewsUser : IdentityUser
---
+++ src/Tests/Sandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sandbox
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
